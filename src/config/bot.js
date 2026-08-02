0fe8d423b2f783f366189e45f43723ede722045c
afnewidevil911-cell <afnewidevil911@gmail.com>
2026-08-01 17:33:52 -0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        states : "I HAVE A BIG DICK",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
